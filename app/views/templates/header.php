29f59d2d8ab538a1af1b595112d112b1392acadc
aatthhuurr <2411500025@mahasiswa.atmaluhur.ac.id>
2025-10-28 08:54:01 +0700

    Ubah Header biar ada garis 3

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,52 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Halaman Mahasiswa</title>
-
-  <!-- Bootstrap CSS -->
-  <link 
-    rel="stylesheet" 
-    href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-  >
-
-  <style>
-    .navbar a,
-    .navbar .nav-link,
-    .navbar .navbar-brand {
-      color: black !important;
-      text-decoration: none;
-    }
-
-    .navbar a:hover,
-    .navbar .nav-link:hover,
-    .navbar .navbar-brand:hover {
-      color: gray !important;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <title>Halaman <?php echo $data['judul']; ?></title>
+    <link rel="stylesheet" href="<?php echo BASEURL; ?>/css/bootstrap.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container">
-    <a class="navbar-brand" href="<?php echo BASEURL; ?>">PWBO</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+    <a class="navbar-brand" href="<?php echo BASEURL; ?>">PWBO Lanjutan</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavaltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?php echo BASEURL; ?>">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo BASEURL; ?>/mahasiswa">Mahasiswa</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo BASEURL; ?>/matakuliah">Mata Kuliah</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo BASEURL; ?>/about">About</a>
-        </li>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="<?php echo BASEURL; ?>">Home<span class="cr-only"></span></a>
+        <a class="nav-item nav-link " href="<?php echo BASEURL; ?>/mahasiswa">Mahasiswa <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link " href="<?php echo BASEURL; ?>/matakuliah">Mata Kuliah<span class="sr-only">(current)</span></a>
+    <a class="nav-item nav-link" href="<?php echo BASEURL; ?>/about">About</a>
     </div>
-  </div>
+    </div>
+</div>
 </nav>
